15dac121cc6927d3922c9e2edc999a3e19c0f75a
Septania <hafidzsyahn@gmail.com>
2025-07-13 15:30:55 +0700

    add : delet isloading detailStatus.ts

--- Circle/src/pages/middlePages/detailStatus.tsx
+++ Circle/src/pages/middlePages/detailStatus.tsx
@@ -41,8 +41,7 @@ export function StatusPage() {
     isLiked,
     handleLikeClick,
     isHovered,
-    setIsHovered,
-    isLoading: likeLoading,
+    setIsHovered
   } = useLike(id || "");
 
   const handleDelete = () => {
